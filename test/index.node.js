104c197d37c58d723c6426c933ad9de9061e4467
uupaa <uupaa.js@gmail.com>
2014-01-30 22:57:12 +0900

    add MappedArray.js

--- test/index.node.js
+++ test/index.node.js
@@ -1,5 +1,5 @@
-require("uupaa.test.js");
 require("uupaa.task.js");
+require("uupaa.test.js");
 require("../lib/PageVisibilityEvent.js");
 require("../lib/PageVisibilityEvent.min.js");
 require("./test.js");
