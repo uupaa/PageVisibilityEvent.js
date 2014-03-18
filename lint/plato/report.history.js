5262faedbd60a3206021c3244460cb8043b577a4
uupaa <uupaa.js@gmail.com>
2014-03-19 05:00:21 +0900

    avoid 'PageVisibilityEvent.js will not work in this environment.' message

--- lint/plato/report.history.js
+++ lint/plato/report.history.js
@@ -1,1 +1,46 @@
-__history = [{"date":"Wed, 12 Mar 2014 21:55:47 GMT","total":{"sloc":153,"maintainability":68.16594995606977},"average":{"sloc":153,"maintainability":"68.17"}},{"date":"Wed, 12 Mar 2014 21:56:24 GMT","total":{"sloc":155,"maintainability":68.04070459087691},"average":{"sloc":155,"maintainability":"68.04"}}]
+__history = [
+  {
+    "date": "Wed, 12 Mar 2014 21:55:47 GMT",
+    "total": {
+      "sloc": 153,
+      "maintainability": 68.16594995606977
+    },
+    "average": {
+      "sloc": 153,
+      "maintainability": "68.17"
+    }
+  },
+  {
+    "date": "Wed, 12 Mar 2014 21:56:24 GMT",
+    "total": {
+      "sloc": 155,
+      "maintainability": 68.04070459087691
+    },
+    "average": {
+      "sloc": 155,
+      "maintainability": "68.04"
+    }
+  },
+  {
+    "date": "Tue, 18 Mar 2014 19:52:59 GMT",
+    "total": {
+      "sloc": 155,
+      "maintainability": 68.04070459087691
+    },
+    "average": {
+      "sloc": 155,
+      "maintainability": "68.04"
+    }
+  },
+  {
+    "date": "Tue, 18 Mar 2014 19:59:03 GMT",
+    "total": {
+      "sloc": 158,
+      "maintainability": 68.03028040979551
+    },
+    "average": {
+      "sloc": 158,
+      "maintainability": "68.03"
+    }
+  }
+]
